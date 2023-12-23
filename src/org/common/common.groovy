f16bef5646e5c843856af0c698ec9bc1d3b17e05
pankaj0129 <133801920+pankaj0129@users.noreply.github.com>
2023-12-23 23:21:08 +0530

    Update common.groovy

--- src/org/common/common.groovy
+++ src/org/common/common.groovy
@@ -1,11 +1,5 @@
 package org.common
 
-class CommonFuncs {
-    def sayHi() {
-        echo "Hi from CommonFuncs!"
-    }
-
-    def sayHello(str) {
-        echo "Hello ${str}"
-    }
+def initialize() {
+    terraform init
 }
